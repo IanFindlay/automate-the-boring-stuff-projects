752d828fb71f20d19cfb56caa5514951683a5b55
IFinners <IDFinners@Gmail.com>
2017-12-29 21:00:11 +0000

    Add Chapter 11 directory and 2048.py

---
+++ Chapter 11/2048.py
@@ -0,0 +1,16 @@
+#!usr/bin/env/ python3
+
+"""Automatically plays the game 2048."""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+browser.get('https://gabrielecirulli.github.io/2048/')
+html_elem = browser.find_element_by_tag_name('html')
+
+while True:
+    html_elem.send_keys(Keys.UP)
+    html_elem.send_keys(Keys.RIGHT)
+    html_elem.send_keys(Keys.DOWN)
+    html_elem.send_keys(Keys.LEFT)
