4ec1d2b8c984b861924aaeb745f91ae9d7e0786c
IFinners <IDFinners@Gmail.com>
2018-01-01 17:56:25 +0000

    Add img_down.py

---
+++ Chapter 11/img_down.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+"""Download jpg/png images from Imgur that match a user's search term(s)."""
+
+import os
+import requests
+import bs4
+
+def image_downloader(extension):
+    """Search for and download all images of the argument type from Imgur."""
+    url = 'http://imgur.com/search?q=' + SEARCH + ' ext:' + extension
+    os.makedirs('/home/finners/Downloads/imgur', exist_ok=True)
+
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    image_elem = soup.select('.post > .image-list-link img')
+
+    for i, image in enumerate(image_elem):
+        # Convert image URL from thumbnail size to fullsize version
+        image_url_s = 'https:' + image_elem[i].get('src')
+        image_url = image_url_s[: -5] + '.' + extension
+
+        print('Downloading image {}'.format(image_url))
+        res = requests.get(image_url)
+        res.raise_for_status()
+        image_file = open(os.path.join('/home/finners/Downloads/imgur',
+                                        os.path.basename(image_url)), 'wb')
+        for chunk in res.iter_content(1000000):
+            image_file.write(chunk)
+        image_file.close()
+
+    return len(image_elem)
+
+
+SEARCH = input('Enter desired search term(s): ')
+DOWNLOADED = image_downloader('jpg') + image_downloader('png')
+
+if DOWNLOADED == 0:
+    print('No images found.')
+
+else:
+    print('All ' + str(DOWNLOADED) + ' files successfully downloaded.')
