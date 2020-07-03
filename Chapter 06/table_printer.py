2662b0d0edd186e754104fca2d763a64166167e7
Harshith <33367724+HarshithInapudi@users.noreply.github.com>
2020-07-03 20:29:21 +0530

    Update table_printer.py
    1) Previously if we added more elements in the first sublist, it raised an "index out of range" error.
    So I added try and except.
    2) If we added more elements in other sublists, it didn't print those elements.
    I added a block of code(lines 20 to 23 and line 29) that solves this issue.

--- Chapter 06/table_printer.py
+++ Chapter 06/table_printer.py
@@ -1,31 +1,38 @@
 """Prints a nicely aligned table from a list of strings."""
 
 table_data = [['apples', 'oranges', 'cherries', 'banana'],
-              ['Alice', 'Bob', 'Carol', 'David'],
-              ['dogs', 'cats', 'moose', 'goose']]
+			  ['Alice', 'Bob', 'Carol', 'David'],
+			  ['dogs', 'cats', 'moose', 'goose']]
 
 
 def print_table(table):
-    """Print a formatted, properly aligned table version of a list."""
-    # Create list with zeroes equal to the length of input list
-    col_widths = [0] * len(table)
+	"""Print a formatted, properly aligned table version of a list."""
+	# Create list with zeroes equal to the length of input list
+	col_widths = [0] * len(table)
 
     # Finds longest word in each sublists and sets col_width[x] to value
-    count = 0
-    while count < len(table):
-        for item in table[count]:
-            if len(item) > col_widths[count]:
-                col_widths[count] = len(item)
-        count += 1
+	for item in range(len(table)):
+		for word in table[item]:
+			if col_widths[item] < len(word):
+				col_widths[item] = len(word)
+	
+	# Finds the longest sublist in the table
+	length = 0
+	for i in range(len(table)):
+		if length < len(table[i]):
+			length = len(table[i])
 
     # Iterates over lists taking nth element from each
     # and printing it with the other nth subitems
     # right aligned according to the corresponding col_width value
 
-    for word in range(len(table[0])):
-        for item in range(len(table)):
-            print(table[item][word].rjust(col_widths[item]), end=' ')
-        print()
+	for word in range(length):
+		for item in range(len(table)):
+			try:
+				print(table[item][word].rjust(col_widths[item]), end=' ')
+			except IndexError:
+				print(' '.rjust(col_widths[item]), end =' ')
+		print()
 
 
 print_table(table_data)
