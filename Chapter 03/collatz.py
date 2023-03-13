9f8fced403303e9b7b46cde03af5629118f903d6
CAG07 <craig.gehrig@gmail.com>
2023-03-13 17:28:09 -0500

    collatz.py

--- Chapter 03/collatz.py
+++ Chapter 03/collatz.py
@@ -4,10 +4,16 @@ def collatz(number):
     if number == 1:
        print("You can't escape the Collatz infite loop!")
     elif number % 2 == 0:
-        print(number/2)
-        collatz(number/2)
+        print(number / 2)
+        collatz(number / 2)
     else:
-        print(number*3 + 1)
-        collatz(number*3 + 1)
+        print(number * 3 + 1)
+        collatz(number * 3 + 1)
 
 
+try:
+    number = int(input('Choose any integer great than 1:'))
+    collatz(number)
+
+except ValueError:
+    print('You must enter an integer')
