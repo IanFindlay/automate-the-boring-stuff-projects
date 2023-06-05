86959e271c7cfef5e99fe201da0d6d3713280472
CAG07 <craig.gehrig@gmail.com>
2023-06-05 12:16:09 -0500

    Add display_inventory function

--- Chapter 05/game_inventory.py
+++ Chapter 05/game_inventory.py
@@ -5,3 +5,10 @@ inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12, '
 
 # This function displays the inventory
 def display_inventory(inventory):
+    print('Inventory:')
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        item_total += v
+    print('Total number of items : ' + str(item_total))
+
